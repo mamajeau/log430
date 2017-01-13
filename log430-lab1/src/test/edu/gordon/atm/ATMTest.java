5f878dcf1afe31455f95f09d7d2ef5437c6b9d07
mamajeau <ma.majeau@gmail.com>
2017-01-13 10:30:31 -0500

    Ajout de tout les shit

---
+++ log430-lab1/src/test/edu/gordon/atm/ATMTest.java
@@ -0,0 +1,38 @@
+package edu.gordon.atm;
+
+import org.junit.Test;
+import org.junit.experimental.categories.Categories;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by maaj on 1/13/2017.
+ */
+public class ATMTest {
+
+
+@Test
+    public void withdrawalTest()
+{
+;
+}
+
+@Test
+    public void depositTest()
+{
+;
+}
+
+@Test
+    public void transferTest()
+{
+;
+}
+
+@Test
+    public void balanceTest()
+{
+;
+}
+
+}
