d3135e450946b8806e4016ab380834b2d58824ae
mamajeau <ma.majeau@gmail.com>
2017-01-13 22:29:38 -0500

    Les tests maven fonction, lets code

---
+++ log430-lab1/src/test/java/edu/gordon/atm/ATMTest.java
@@ -0,0 +1,39 @@
+package edu.gordon.atm;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.experimental.categories.Categories;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by maaj on 1/13/2017.
+ */
+public class ATMTest {
+
+
+@Test
+    public void withdrawalTest()
+{
+    Assert.assertEquals(true,true);
+}
+
+@Test
+    public void depositTest()
+{
+;
+}
+
+@Test
+    public void transferTest()
+{
+;
+}
+
+@Test
+    public void balanceTest()
+{
+;
+}
+
+}
